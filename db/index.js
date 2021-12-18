dce2767694fc8d9a490ab374d3b8d7414bb6f0cf
Ruben Gonzalez <rubengon.uk@gmail.com>
2021-12-18 20:31:37 +0000

    Task2-a completed

---
+++ db/index.js
@@ -0,0 +1,18 @@
+
+import pg from "pg";
+
+const pool = new pg.Pool(
+  {
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+}
+);
+
+function query(text, params) {
+    return pool.query(text, params )
+}
+
+export default query;
